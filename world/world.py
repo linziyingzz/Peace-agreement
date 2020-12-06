fea1d875ceed97d1d74071b05506896f46380bc8
Xiaoying Wang <xiaoying.wang-5@student.manchester.ac.uk>
2020-12-06 19:42:48 +0800

    Select useful columns

--- world/world.py
+++ world/world.py
@@ -11,3 +11,18 @@ file_2019 = pd.read_csv("2019.csv")
 
 final_data = pd.concat([file_2017, file_2018, file_2019], axis=0)
 
+country_list=pd.unique(final_data['country'])
+type_list=pd.unique(final_data['event_type'])
+
+type_count_data=pd.DataFrame(columns=type_list)
+
+
+# Go through every single type of the conflicts in every country
+# And make a count
+index = 0
+for country in country_list:
+    for type_name in type_list:
+        count=final_data[(final_data['country']==country) & (final_data['event_type']==type_name)].shape[0]
+        type_count_data.loc[index, type_name] = count
+    index += 1
+type_count_data.head()
